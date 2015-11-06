7c221c3693df3aaab3422e5c1468b4f769562ccb
Malhoura <malhoura@hawk.iit.edu>
2015-11-06 12:43:37 -0600

    changed

--- result.php
+++ result.php
@@ -1,94 +1,113 @@
 <?php
+echo "Hello World1";
 session_start();
-// In PHP versions earlier than 4.1.0, $HTTP_POST_FILES should be used instead
-// of $_FILES.
+var_dump($_POST);
+if(!empty($_POST)){
 echo $_POST['useremail'];
+echo $_POST['phone'];
+}
+else
+{
+echo "post empty";
+}
 $uploaddir = '/tmp/';
 $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
-echo '<pre>';
+print '<pre>';
 if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-    echo "File is valid, and was successfully uploaded.\n";
+  echo "File is valid, and was successfully uploaded.\n";
 } else {
     echo "Possible file upload attack!\n";
 }
-
 echo 'Here is some more debugging info:';
 print_r($_FILES);
 print "</pre>";
-
+  
 require 'vendor/autoload.php';
+#use Aws\S3\S3Client;
+#$client = S3Client::factory();
 $s3 = new Aws\S3\S3Client([
     'version' => 'latest',
     'region'  => 'us-east-1'
 ]);
-
-$bucket = uniqid("php-malhoura-",false);
-
-# AWS PHP SDK version 3 create bucket
+#print_r($s3);
+$bucket = uniqid("mp1Sneha",false);
+#$result = $s3->createBucket(array(
+#    'Bucket' => $bucket
+#));
+#
+## AWS PHP SDK version 3 create bucket
 $result = $s3->createBucket([
     'ACL' => 'public-read',
     'Bucket' => $bucket
 ]);
-
-
-# PHP version 3
+#print_r($result);
 $result = $s3->putObject([
     'ACL' => 'public-read',
     'Bucket' => $bucket,
    'Key' => $uploadfile,
-   'ContentType' => $_FILES['userfile']['type'],
-   'Body' => fopen($uploadfile, 'r+')
-]);  
-
+'ContentType' => $_FILES['userfile']['type'],
+'Body' => fopen($uploadfile,'r+')
+]);
 $url = $result['ObjectURL'];
 echo $url;
-
 $rds = new Aws\Rds\RdsClient([
     'version' => 'latest',
     'region'  => 'us-east-1'
 ]);
-
-$result = $rds->describeDBInstances([
-    'DBInstanceIdentifier' => 'malhoura-mp1',
-]);
-
+$result = $rds->describeDBInstances(array(
+    'DBInstanceIdentifier' => 'MP1'
+   
+));
 $endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
-    echo "============\n". $endpoint . "================";^M
-//echo "begin database";^M
-$link = mysqli_connect($endpoint,"malhoura","malhoura","users") or die("Error " . mysqli_error($link));
-/* check connection */
+    echo "============\n". $endpoint . "================";
+$link = mysqli_connect($endpoint,"testconnection1","testconnection1","Project1");
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
-/* Prepared statement, stage 1: prepare */
-if (!($stmt = $link->prepare("INSERT INTO User (id,username, useremail,telephone,filename,s3rawurl,s3finishedurl,status,date) VALUES (NULL,?,?,?,?,?,?,?,?)"))) {
+else {
+echo "Success";
+}
+if (!($stmt = $link->prepare("INSERT INTO MiniProject1 (uname,email,phoneforsms,raws3url,finisheds3url,jpegfilename,state) VALUES (?,?,?,?,?,?,?)"))) {
     echo "Prepare failed: (" . $link->errno . ") " . $link->error;
 }
-$username = $_POST['username'];
-$useremail = $_POST['useremail'];
-$telephone = $_POST['telephone'];
-$s3rawurl = $url; //  $result['ObjectURL']; from above
-$filename = basename($_FILES['userfile']['name']);
-$s3finishedurl = "none";
-$status =0;
-$date='2009-04-30 10:09:00';
-$stmt->bind_param("ssssssis",$username,$email,$phone,$filename,$s3rawurl,$s3finishedurl,$status,$date);
+$uname="Sneha";
+$email = $_POST['useremail'];
+$phoneforsms = $_POST['phone'];
+$raws3url = $url; 
+$finisheds3url = "none";
+$jpegfilename = basename($_FILES['userfile']['name']);
+$state=0;
+$stmt->bind_param("ssssssi",$uname,$email,$phoneforsms,$raws3url,$finisheds3url,$jpegfilename,$state);
 if (!$stmt->execute()) {
     echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
 }
 printf("%d Row inserted.\n", $stmt->affected_rows);
-/* explicit close recommended */
 $stmt->close();
-$link->real_query("SELECT * FROM User");
+$link->real_query("SELECT * FROM MiniProject1");
 $res = $link->use_result();
 echo "Result set order...\n";
 while ($row = $res->fetch_assoc()) {
-    echo $row['id'] . " " . $row['username'] . " " . $row['useremail']. " " . $row['telephone'];
+    echo $row['id'] . " " . $row['email']. " " . $row['phoneforsms'];
 }
 $link->close();
-//add code to detect if subscribed to SNS topic 
-//if not subscribed then subscribe the user and UPDATE the column in the database with a new value 0 to 1 so that then each time you don't have to resubscribe them
-// add code to generate SQS Message with a value of the ID returned from the most recent inserted piece of work
-//  Add code to update database to UPDATE status column to 1 (in progress)
-?>
+?> 
+
+  
+
+
+
+
+##foreach ($result->getPath('DBInstances/*/Endpoint/Address') as $ep){
+##echo $ep;
+##$endpoint=$ep;
+##} 
+#
+
+#
+#/* Prepared statement, stage 1: prepare */
+#//add code to detect if subscribed to SNS topic 
+#//if not subscribed then subscribe the user and UPDATE the column in the database with a new value 0 to 1 so that then each time you don't have to resubscribe them
+#// add code to generate SQS Message with a value of the ID returned from the most recent inserted piece of work
+#//  Add code to update database to UPDATE status column to 1 (in progress)
+#?>   
