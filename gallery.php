25a2ec5e66e80f5ec23794c00afba871bf51c70d
Malhoura <malhoura@hawk.iit.edu>
2015-11-09 20:31:42 -0600

    edited gallery

--- gallery.php
+++ gallery.php
@@ -5,20 +5,22 @@
 <div>
 <?php
 session_start();
-$emailaddress = $_POST["email"];
-echo $emailaddress;
+$useremail = $_POST["useremail"];
+echo $useremail;
 require 'vendor/autoload.php';
 $rds = new Aws\Rds\RdsClient([
     'version' => 'latest',
     'region'  => 'us-east-1'
 ]);
+
 $result = $rds->describeDBInstances(array(
     'DBInstanceIdentifier' => 'malhorua-mp1'
    
 ));
+
 $endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
     echo "============\n". $endpoint . "================";
-$link = mysqli_connect($endpoint,"malhoura","malhoura","Project1");
+$link = mysqli_connect($endpoint,"malhoura","malhoura","users");
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
@@ -26,12 +28,12 @@ if (mysqli_connect_errno()) {
 else {
 echo "Success";
 }
-$link->real_query("SELECT * FROM User WHERE email = '$emailaddress'");
+$link->real_query("SELECT * FROM User WHERE useremail = '$useremail'");
 $res = $link->use_result();
 echo "Result set order...\n";
 while ($row = $res->fetch_assoc()) {
     echo "<img src =\" " . $row['raws3url'] . "\" /><img src =\"" .$row['finisheds3url'] . "\"/>";
-echo $row['id'] . "Email: " . $row['emailaddress'];
+echo $row['id'] . "Email: " . $row['useremaail'];
 }
 $link->close();
 ?>
