8b5707748155d0e4c6be48dad332548f51f5eec8
malhoura <malhoura@hawk.iit.edu>
2015-11-22 14:38:48 -0600

    edited table creation

--- setup.php
+++ setup.php
@@ -1,26 +1,28 @@
 <?php
 // Start the session
-require '/var/www/html/vendor/autoload.php';
+require 'vendor/autoload.php';
 $rds = new Aws\Rds\RdsClient([
     'version' => 'latest',
     'region'  => 'us-east-1'
 ]);
 
 // Create a table 
-$result = $rds->describeDBInstances([
-    'DBInstanceIdentifier' => 'malhoura-mp1',
-]);
+$result = $rds->describeDBInstances(array(
+    'DBInstanceIdentifier' => 'malhoura-mp1'
+));
 
-$rds->waituntil('DBInstanceAvailable',[DBInstanceIdentifier' => 'malhoura-mp1,]);
 
 $endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
 print "============\n". $endpoint . "================\n";
-$link = mysqli_connect($endpoint,"malhoura","malhoura","malhouradb",3306) or die("Error " . mysqli_error($link)); 
+$link = new mysqli($endpoint,"malhoura","malhoura","malhouradb",3306) or die("Error " . mysqli_error($link)); 
 echo "Here is the result: " . $link;
 
+$sql = "DROP TABLE IF EXISTS User";
+if(!mysqli_query($link, $sql)) {
+   echo "Error : " . mysqli_error($link);
+} 
 
-
-$sql = 'CREATE TABLE IF NOT EXISTS User  
+$link->query("CREATE TABLE User  
 (
 ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 username VARCHAR(20),
@@ -30,10 +32,9 @@ raws3url VARCHAR(256),
 finisheds3url VARCHAR(256),
 filename VARCHAR(256),
 state TINYINT(3),
-datetime VARCHAR 
+datetime timestamp 
+)");
 
-)';
-$link->query($sql);
 shell-exec("chmod 600 setup.php");
 
 ?>
